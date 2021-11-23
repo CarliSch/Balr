d4bc82db57e877ae608a5f34984aa8880f98cb72
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-23 11:36:41 +0100

    have added the has_many and belongs_to relationships as well as validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,6 @@
 class Match < ApplicationRecord
+  has_many :match_users
+  has_many :users, through: :match_users
+
+  validates :location, :start_at, presence: true
 end
--- app/models/match_user.rb
+++ app/models/match_user.rb
@@ -1,5 +1,7 @@
 class MatchUser < ApplicationRecord
   belongs_to :match
   belongs_to :user
-  belongs_to :club
+  belongs_to :club, optional: true
+
+  validates :team, inclusion: {in: ["A", "B"]}
 end
