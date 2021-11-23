ce62bb62ce916ac46943bbe06907e53387361d75
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-23 12:03:52 +0100

    routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :matches, only: [:index, :show, :new, :create] do
+    resources :match_users, only: [:create]
+  end
+
+  resource :profile, only: [:show]
+  resources :user, only: [:show]
+  resources :clubs, only: [:index, :show, :new, :create] do
+    resources :club_requests, only: [:create]
+  end
+  resources :club_requests, only: [:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
