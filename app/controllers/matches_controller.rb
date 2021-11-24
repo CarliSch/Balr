27e0f24762ecbfca53345b1392e2e9b57d71b61c
Ruben Asmus <ruben.berlin@icloud.com>
2021-11-24 18:06:07 +0100

    final match index

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,7 @@
 class MatchesController < ApplicationController
 
   def index
-    @matches = Match.upcoming
+    @matches = Match.upcoming.order(:start_at)
     @matches_by_date = @matches.group_by do |match|
       match.start_at.to_date
     end
