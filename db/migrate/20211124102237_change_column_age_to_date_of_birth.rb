011cf1e463474b018086914d4bea35ee04e1772d
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-24 14:48:36 +0100

    some more stuff

--- db/migrate/20211124102237_change_column_age_to_date_of_birth.rb
+++ db/migrate/20211124102237_change_column_age_to_date_of_birth.rb
@@ -1,6 +1,6 @@
 class ChangeColumnAgeToDateOfBirth < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :age, :integer
-    dd_column :users, :birthday, :date
+    add_column :users, :birthday, :date
   end
 end
