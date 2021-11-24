c700ed07724cdd880c73489cfc7581f6d7ed5c80
Ruben Asmus <ruben.berlin@icloud.com>
2021-11-24 16:20:00 +0100

    updated match index

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,12 @@ class Match < ApplicationRecord
   has_many :users, through: :match_users
 
   validates :location, :start_at, presence: true
+
+  def spots_left
+    max_spots - match_users.count
+  end
+
+  def max_spots
+    22
+  end
 end
