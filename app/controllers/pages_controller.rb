48320cc49f1501939a6a818a5e8a6bdce68ff2ca
AugustWanger <j.augustwanger@gmail.com>
2021-11-25 12:17:49 +0100

    completed the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @matches = Match.upcoming
+    @matches = Match.upcoming.order(:start_at)
+    @next_match = @matches.first
     @matches_by_date = @matches.group_by do |match|
       match.start_at.to_date
     end
+    @user_next_matches = current_user.matches.upcoming.order(:start_at).limit(3) unless user_signed_in?
   end
 end
