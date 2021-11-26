aaa20ce679b0a877508f8f281a16ca4324d44c8d
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-26 14:36:40 +0100

    avatar and other stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthday, :city, :email, :password, :position, :photo])
   end
 end
