a4f37ed4534d118b73d49cdf0039b4ff6be7483c
Ruben Asmus <ruben.berlin@icloud.com>
2021-11-29 14:51:20 +0100

    started fixing profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :birthday, :city, :position, presence: true
   has_one_attached :photo
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
 end
