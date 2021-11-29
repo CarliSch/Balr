9673e6037910d2e8dc9ac2cf7e1de777829fab75
Ruben Asmus <ruben.berlin@icloud.com>
2021-11-29 17:42:19 +0100

    added pictures and css to the profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
+  POSITIONS = [
+    "Goalkeeper",
+    "Right Full-back",
+    "Left Full-back",
+    "Center-back",
+    "Sweeper",
+    "Defensive Midfielder",
+    "Right Midfielder",
+    "Center Midfielder",
+    "Striker",
+    "Attacking Midfielder",
+    "Left Midfielder"
+
+  ]
+
+  extend Enumerize
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,10 +24,31 @@ class User < ApplicationRecord
   has_many :matches, through: :match_users
 
   validates :first_name, :last_name, :birthday, :city, :position, presence: true
+  validates :position, inclusion: { in: POSITIONS }
   has_one_attached :photo
+
+  enumerize :position, in: POSITIONS
 
   def age
     now = Time.now.utc.to_date
     now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
+  def position_picture
+    case position
+    when "Striker" then "goalkeeper.jpg"
+    when "Goalkeeper" then "goalkeeper.jpg"
+    when "Right Full-back" then "striker.jpg"
+    when "Left Full-back" then "defender_midfield.jpg"
+    when "Center-back" then "defender_midfield.jpg"
+    when "Sweeper" then "defender_midfield.jpg"
+    when "Defensive Midfielder" then "defender_midfield.jpg"
+    when "Right Midfielder" then "defender_midfield.jpg"
+    when "Center Midfielder" then "defender_midfield.jpg"
+    when "Center Forward" then "defender_midfield.jpg"
+    when "Left Midfielder" then "defender_midfield.jpg"
+    else
+      "footballpitch.jpg"
+    end
   end
 end
