092b96c7a7133908ae32327b99bb0127e5b0a912
AugustWanger <j.augustwanger@gmail.com>
2021-11-30 11:47:50 +0100

    added the create request method

--- app/controllers/club_requests_controller.rb
+++ app/controllers/club_requests_controller.rb
@@ -1,7 +1,7 @@
 class ClubRequestsController < ApplicationController
-
   def create
-
+    @club = Club.find(params[:club_id])
+    @club_request = ClubRequest.create(user: current_user, club: @club, status: "pending")
+    redirect_to clubs_path
   end
-  
 end
