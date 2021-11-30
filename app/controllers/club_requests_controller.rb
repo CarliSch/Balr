56be17f13afc82430be5c4b44418c5c59cd06232
AugustWanger <j.augustwanger@gmail.com>
2021-11-30 12:44:15 +0100

    worked on the join request which now sends requests/still need to do the accept and reject

--- app/controllers/club_requests_controller.rb
+++ app/controllers/club_requests_controller.rb
@@ -1,7 +1,7 @@
 class ClubRequestsController < ApplicationController
   def create
     @club = Club.find(params[:club_id])
-    @club_request = ClubRequest.create(user: current_user, club: @club, status: "pending")
-    redirect_to clubs_path
+    @club_request = ClubRequest.create(user_id: current_user.id, club_id: @club.id)
+    redirect_to club_path(@club)
   end
 end
