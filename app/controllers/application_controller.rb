c825f785893dbeafb8782a7d6f0071598ede7773
AugustWanger <j.augustwanger@gmail.com>
2021-12-01 10:30:17 +0100

    Title and meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthday, :city, :email, :password, :position, :photo])
   end
+
+  def default_url_options
+  { host: ENV["www.balrsport.com"] || "localhost:3000" }
+  end
+
 end
