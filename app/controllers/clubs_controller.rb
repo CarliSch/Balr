f9a2776ba2018077081136b4d8e8d450c0dbfb01
AugustWanger <j.augustwanger@gmail.com>
2022-01-19 14:58:31 +0100

    added back my env file

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,7 +1,6 @@
 class ClubsController < ApplicationController
   def index
     @clubs = Club.where.not(user: current_user)
-    @clubs = policy_scope(Club).upcoming.order(:start_at))
   end
 
   def new
