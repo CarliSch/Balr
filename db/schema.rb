5038cc0a5cba6875ee41c2c707187c83b8847541
AugustWanger <j.augustwanger@gmail.com>
2022-02-14 15:17:54 +0100

    started working on adding teams that have joined in to the tournament groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_123413) do
+ActiveRecord::Schema.define(version: 2022_02_14_120059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2022_02_13_123413) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "challenges", force: :cascade do |t|
@@ -81,11 +93,11 @@ ActiveRecord::Schema.define(version: 2022_02_13_123413) do
 
   create_table "creators", force: :cascade do |t|
     t.string "name"
-    t.string "location"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "location"
     t.index ["user_id"], name: "index_creators_on_user_id"
   end
 
@@ -121,6 +133,7 @@ ActiveRecord::Schema.define(version: 2022_02_13_123413) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "tournament_id", null: false
+    t.text "group", default: [], array: true
     t.index ["tournament_id"], name: "index_tournament_groups_on_tournament_id"
   end
 
