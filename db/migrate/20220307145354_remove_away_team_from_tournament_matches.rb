c0fb94b39cf7164b3889c901e4d770dde5a85ba2
August Wanger <91264080+AugustWanger@users.noreply.github.com>
2022-03-07 15:57:16 +0100

    Revert "b"

--- db/migrate/20220307145354_remove_away_team_from_tournament_matches.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAwayTeamFromTournamentMatches < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :tournament_matches, :home_team, null: false, foreign_key: true
-  end
-end
