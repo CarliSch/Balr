5598166abdd1b974bfb37024e09460221755d84c
AugustWanger <j.augustwanger@gmail.com>
2022-03-09 14:06:23 +0100

    changed the tournament request controller

--- app/controllers/tournament_requests_controller.rb
+++ app/controllers/tournament_requests_controller.rb
@@ -17,38 +17,38 @@ class TournamentRequestsController < ApplicationController
     @tournament = @tournament_request.tournament
     @tournament_group = @tournament.tournament_groups.first
     @club = @tournament_request.club
-    if @tournament_group.bracket.size == 3
-      @tournament_group.bracket << @club.id
+    if @tournament_group.tournament_clubs.size == 3
+      TournamentClub.create!(club: @club, tournament_group: @tournament_group, points: 0)
       n = 1
       c = 2
       3.times.map do
-        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[0]), away_team: Club.find(@tournament_group.bracket[n]))
+        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[0].club.id), away_team: Club.find(@tournament_group.tournament_clubs[n].club.id))
         n += 1
       end
       2.times.map do
-        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[1]), away_team: Club.find(@tournament_group.bracket[c]))
+        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[1].club.id), away_team: Club.find(@tournament_group.tournament_clubs[c].club.id))
         c += 1
       end
-      @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[2]), away_team: Club.find(@tournament_group.bracket[3]))
+      @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[2].club.id), away_team: Club.find(@tournament_group.tournament_clubs[3].club.id))
 
-    elsif @tournament_group.bracket.size == 4
+    elsif @tournament_group.tournament_clubs.size == 4
       @tournament_group = TournamentGroup.find(@tournament_group.id + 1)
-      @tournament_group.bracket << @club.id
-      if @tournament_group.bracket.size == 4
+      TournamentClub.create!(club: @club, tournament_group: @tournament_group, points: 0)
+      if @tournament_group.tournament_clubs.size == 4
         n = 1
         c = 2
         3.times.map do
-          @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[0]), away_team: Club.find(@tournament_group.bracket[n]))
+          @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[0].club.id), away_team: Club.find(@tournament_group.tournament_clubs[n].club.id))
           n += 1
         end
         2.times.map do
-          @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[1]), away_team: Club.find(@tournament_group.bracket[c]))
+          @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[1].club.id), away_team: Club.find(@tournament_group.tournament_clubs[c].club.id))
           c += 1
         end
-        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.bracket[2]), away_team: Club.find(@tournament_group.bracket[3]))
+        @tournament_match = TournamentMatch.create!(tournament_group: @tournament_group, home_team: Club.find(@tournament_group.tournament_clubs[2].club.id), away_team: Club.find(@tournament_group.tournament_clubs[3].club.id))
       end
     else
-    @tournament_group.bracket << @club.id
+    TournamentClub.create!(club: @club, tournament_group: @tournament_group, points: 0)
     end
 
     @tournament_group.save!
--- app/models/tournament_club.rb
+++ app/models/tournament_club.rb
@@ -1,6 +1,6 @@
 class TournamentClub < ApplicationRecord
   belongs_to :tournament_group
-  belongs_to :tournament_knockout
+  belongs_to :tournament_knockout, optional: true
   belongs_to :club
   has_many :club_tournament_matches
 end
