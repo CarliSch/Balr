0e9cb9843f0cdd55600b35687c6e068d787aded0
AugustWanger <j.augustwanger@gmail.com>
2022-03-10 14:42:49 +0100

    added register score

--- app/controllers/tournament_matches_controller.rb
+++ app/controllers/tournament_matches_controller.rb
@@ -11,8 +11,17 @@ class TournamentMatchesController < ApplicationController
     @tournament_match = TournamentMatch.find(params[:id])
     @home_team = @tournament_match.home_team
     @away_team = @tournament_match.away_team
-    
 
+    if @home_team.goals > @away_team.goals
+      @home_team.tournament_club.update!(points: @home_team.tournament_club.points += 3)
+    elsif @home_team.goals < @away_team.goals
+      @away_team.tournament_club.update!(points: @away_team.tournament_club.points += 3)
+    else
+      @away_team.tournament_club.update!(points: @away_team.tournament_club.points += 1)
+      @home_team.tournament_club.update!(points: @home_team.tournament_club.points += 1)
+    end
+    redirect_to tournament_group_tournament_match_path(@tournament_match.tournament_group, @tournament_match)
+    @tournament_match.save!
     authorize @tournament_match
   end
 
--- app/models/tournament_match.rb
+++ app/models/tournament_match.rb
@@ -3,12 +3,5 @@ class TournamentMatch < ApplicationRecord
   belongs_to :home_team
   belongs_to :away_team
   has_many :club_tournament_matches
-
-  def home_team_goals
-    0
-  end
-
-  def away_team_goals
-    0
-  end
+  
 end
