f0a8dba7393986462e01bc64a09da97f358a0e92
AugustWanger <j.augustwanger@gmail.com>
2022-03-10 14:58:57 +0100

    added gorup structure

--- app/models/tournament_group.rb
+++ app/models/tournament_group.rb
@@ -4,12 +4,6 @@ class TournamentGroup < ApplicationRecord
   has_many :tournament_clubs
 
   def standings
-    @standings = []
-    @tournament_clubs ||= TournamentClub.where(tournament_group: self)
-    @tournament_clubs.each do |club|
-      @standings << club.points
-    end
-    @standings = @standings.sort
-    @standings.reverse
+    @tournament_clubs ||= TournamentClub.where(tournament_group: self).order(points: :desc)
   end
 end
