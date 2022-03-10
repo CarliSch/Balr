524634b514faff84a08fdbb34623fb7223bdb92f
Ruben Asmus <ruben.berlin@icloud.com>
2022-03-10 14:24:00 +0100

    changed the creator cards

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2022_02_27_124910) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "challenges", force: :cascade do |t|
     t.string "status"
     t.bigint "challenged_club_id"
@@ -57,6 +69,12 @@ ActiveRecord::Schema.define(version: 2022_02_27_124910) do
   end
 
   create_table "club_tournament_matches", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "club_tournaments", force: :cascade do |t|
+    t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -141,7 +159,9 @@ ActiveRecord::Schema.define(version: 2022_02_27_124910) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "club_id", null: false
     t.bigint "tournament_id", null: false
+    t.bigint "creator_id", null: false
     t.index ["club_id"], name: "index_tournament_requests_on_club_id"
+    t.index ["creator_id"], name: "index_tournament_requests_on_creator_id"
     t.index ["tournament_id"], name: "index_tournament_requests_on_tournament_id"
   end
 
@@ -190,6 +210,7 @@ ActiveRecord::Schema.define(version: 2022_02_27_124910) do
   add_foreign_key "tournament_knockouts", "tournament_groups"
   add_foreign_key "tournament_matches", "tournament_groups"
   add_foreign_key "tournament_requests", "clubs"
+  add_foreign_key "tournament_requests", "creators"
   add_foreign_key "tournament_requests", "tournaments"
   add_foreign_key "tournaments", "creators"
 end
